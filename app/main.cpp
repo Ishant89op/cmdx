e2d0e32ca493b0eeee26ebda663c4fd93ca4c14e
Ishant89op <ishant898734@gmail.com>
2026-09-24 09:27:51 +0530

    Remove 'cmdx ' prefix from version output

--- app/main.cpp
+++ app/main.cpp
@@ -340,9 +340,9 @@ static int run_gui_mode(const std::string& cmd_name, int argc, char* argv[], boo
 
 static void print_version() {
 #ifdef CMDX_VERSION
-    std::cout << "cmdx " << CMDX_VERSION << std::endl;
+    std::cout << CMDX_VERSION << std::endl;
 #else
-    std::cout << "cmdx 1.0.2" << std::endl;
+    std::cout << "1.0.2" << std::endl;
 #endif
 }
 
